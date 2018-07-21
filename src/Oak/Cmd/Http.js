8ca3957a0d7edddd778133e8d1ac64b4b59cd85c
Desmond Rawls <captaingrover@gmail.com>
2018-07-20 17:11:26 -0700

    WIP struggling with http.

--- src/Oak/Cmd/Http.js
+++ src/Oak/Cmd/Http.js
@@ -1,6 +1,18 @@
-exports.getImpl = function(left, right, url, decoder) {
+exports.fetchImpl = function(left, right, url, options, decoder) {
   return function(handler) {
-    fetch(url).then(function(resp) {
+    fetch(url, options).then(function(resp) {
+      return resp.text();
+    }).then(function(resp) {
+      handler(decoder(right(resp)))();
+    }).catch(function(err) {
+      handler(decoder(left(String(err))))();
+    });
+  };
+};
+
+exports.getImpl = function(left, right, url, options, decoder) {
+  return function(handler) {
+    fetch(url, options).then(function(resp) {
       return resp.text();
     }).then(function(resp) {
       handler(decoder(right(resp)))();
--- src/Oak/Cmd/Http.purs
+++ src/Oak/Cmd/Http.purs
@@ -1,6 +1,7 @@
 module Oak.Cmd.Http
   ( HTTP
   , defaultDecode
+  , defaultEncode
   , get
   ) where
 
@@ -8,12 +9,13 @@ import Prelude (show, ($))
 
 import Control.Monad.Except (runExcept)
 import Data.Generic.Rep (class Generic)
-import Data.Foreign.Class (class Decode)
-import Data.Foreign.Generic (defaultOptions, genericDecode, genericDecodeJSON)
-import Data.Foreign.Generic.Class (class GenericDecode)
+import Data.Foreign.Class (class Decode, class Encode)
+import Data.Foreign.Generic (defaultOptions, genericDecode, genericDecodeJSON, genericEncode, genericEncodeJSON)
+import Data.Foreign.Generic.Class (class GenericEncode, class GenericDecode)
 import Data.Foreign.Generic.Types (Options)
 import Data.Foreign (F, Foreign)
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 import Data.Function.Uncurried
   ( Fn4
   , runFn4
@@ -23,9 +25,60 @@ import Oak.Cmd
 
 foreign import data HTTP :: Command
 
+type HttpOptions a
+  = Array (HttpOption a)
+
+data Headers
+  = Array Header
+
+data MediaType
+  = ApplicationJSON
+  | ApplicationXML
+  | TextHTML
+  | TextPlain
+
+data Header
+  = ContentType MediaType
+  | Accept MediaType
+  | Authorization String
+
+data Credentials
+  = CredentialsOmit
+  | CredentialsSameOrigin
+  | CredentialsInclude
+
+data HttpOption a
+  = POST { body :: a }
+  | Headers
+  | Credentials
+
+encodeOptions :: Options
+encodeOptions = defaultOptions { unwrapSingleConstructors = true }
+
+defaultEncode ::
+  ∀ a rep.
+    Generic a rep
+      => GenericEncode rep
+      => a
+      -> Foreign
+defaultEncode = genericEncode encodeOptions
+
+-- #genericEncodeJSON calls JSON.stringify, which throws an exception for
+-- recursive data structures and returns undefined for functions. We can
+-- assume that recursive data structures are impossible given Purescript's
+-- immutability constraint. It is still possible to pass a function to
+-- #genericeEncodeJSON so we should return a Maybe or Either.
+-- TODO: avoid returning undefined.
+makeEncoder :: ∀ a t.
+  Generic a t
+    => GenericEncode t
+    => Encode String
+    => a
+    -> String
+makeEncoder structured = genericEncodeJSON encodeOptions structured
+
 decodeOptions :: Options
 decodeOptions = defaultOptions { unwrapSingleConstructors = true }
-
 
 defaultDecode ::
   ∀ a rep.
@@ -34,7 +87,6 @@ defaultDecode ::
       => Foreign
       -> F a
 defaultDecode = genericDecode decodeOptions
-
 
 makeDecoder :: ∀ a t.
   Generic a t
@@ -47,7 +99,6 @@ makeDecoder json =
     Left err -> Left (show err)
     Right result -> Right result
 
-
 foreign import getImpl :: ∀ c m a.
   Fn4
     (String -> Either String a)
@@ -55,7 +106,6 @@ foreign import getImpl :: ∀ c m a.
     String
     (Either String a -> m)
     (Cmd (http :: HTTP | c) m)
-
 
 get :: ∀ c m a t.
   Generic a t
@@ -65,6 +115,26 @@ get :: ∀ c m a t.
     -> (Either String a -> m)
     -> Cmd (http :: HTTP | c) m
 get url msgCtor = (runFn4 getImpl) Left Right url f
+  where
+    f (Left err) = msgCtor $ Left err
+    f (Right str) = msgCtor $ makeDecoder str
+
+foreign import fetchImpl :: ∀ c m a.
+  Fn4
+    (String -> Either String a)
+    (a -> Either String a)
+    String
+    (Either String a -> m)
+    (Cmd (http :: HTTP | c) m)
+
+fetch :: ∀ c m a t.
+  Generic a t
+    => GenericDecode t
+    => Decode a
+    => String
+    -> (Either String a -> m)
+    -> Cmd (http :: HTTP | c) m
+fetch url msgCtor = (runFn4 getImpl) Left Right url f
   where
     f (Left err) = msgCtor $ Left err
     f (Right str) = msgCtor $ makeDecoder str
